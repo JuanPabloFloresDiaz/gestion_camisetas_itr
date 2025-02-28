7085206db919f601c1098bc9146f1c9073463953
Juan Pablo Flores Díaz <101837425+JuanPabloFloresDiaz@users.noreply.github.com>
2025-02-28 08:57:45 -0600

    Quitando los modulos de auditoria que estaban dando problemas de momento (Se restableceran después si es necesario)

--- apps/api/src/main/java/com/server/api/ApiApplication.java
+++ apps/api/src/main/java/com/server/api/ApiApplication.java
@@ -5,10 +5,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.context.annotation.ComponentScan;
 @EnableJpaAuditing
-@EnableJpaRepositories(basePackages = "com.server.infrastructure.persistence.jpa") 
+@EnableJpaRepositories(basePackages = {
+    "com.server.infrastructure.persistence.jpa", // Repositorios JPA
+    "com.server.domain.repository" // Puertos de dominio
+})
+@EntityScan(basePackages = "com.server.domain.model") // Entidades
+@ComponentScan(basePackages = {
+    "com.server.api", // Controladores y configuraciones en api
+    "com.server.application", // Servicios
+    "com.server.infrastructure" // Implementaciones de repositorios y otras configuraciones
+})
 @SpringBootApplication
-@EntityScan(basePackages = "com.server.domain.model")
 public class ApiApplication {
 
 	public static void main(String[] args) {
